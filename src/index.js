b22cf1e239792704431b31771ea5d29bc1bcff63
saumya <2saumyaray@gmail.com>
2016-04-27 15:51:54 +0530

    the TODO APP is done

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoAppReducers from './reducers'
-import App from './components/App'
+import TodoApp from './components/TodoApp'
 
 let store = createStore(todoAppReducers)
 
 const rootEL = document.getElementById('react-app');
 render(
   <Provider store={store}>
-    <App />
+    <TodoApp />
   </Provider>,
   rootEL
 )
