e015fc405a23f1e371f7b33040f3c284e84a1a28
saumya <2saumyaray@gmail.com>
2016-05-02 16:02:42 +0530

    fixing my filter

--- src/reducers/myFilter.js
+++ src/reducers/myFilter.js
@@ -1,12 +1,17 @@
 // written and Example Reducer which does nothing
 const myFilter = (state=[], action) => {
-  
   console.group('reducers : myFilter : myFilter');
   console.log('myFilter : action',action);
   console.log('myFilter : state',state);
+  var a = 0;
+  if (state.called >= 0) {
+    a = state.called + 1;
+  }
+  var newState = {myFilterVersion:0.1,called:a};
+  console.log('myFilter : state',state);
   console.groupEnd();
 
-  return state
+  return newState;
 }
 
 export default myFilter
